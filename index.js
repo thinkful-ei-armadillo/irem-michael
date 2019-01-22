bddb84c46888f012fcfb5d8953992422234d3e8b
Irem Sen <secilreel@yahoo.com>
2019-01-22 12:14:33 -0500

    js file

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const questions = [
+  {text: 'Question1?',
+    answers: ['ans1', 'ans2','ans3', 'ans4'],
+    correctans: 'ans1'},
+  {text: 'Question2?',
+    answers: ['ans1', 'ans2','ans3', 'ans4'],
+    correctans: 'ans4'}
+];
+// const STORE = {
+//     number: 1,
+//     ansTyped: 'ans1',
+//     anscorrect: 'ans2',};
+
+function generateAnswerList(answers){
+}
+function renderQuestionText(){
+  //renders the question page
+  
+}
+function handleAnswerSubmit(){
+  $().on()
+}
+
+$(handleAnswerSubmit);
